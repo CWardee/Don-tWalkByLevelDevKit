a83a84eec3de237f72539945356c4bfb305bf71a
CWardee <chris@cwardee.co.uk>
2020-02-12 12:35:29 +0000

    Version1

---
+++ Don't Walk By - DevKit/Tiled/tiled-windows-64bit-snapshot/TileSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="2020.01.22" name="TileSet" tilewidth="64" tileheight="64" tilecount="32" columns="4">
+ <image source="../../TileSet.png" width="256" height="512"/>
+</tileset>
